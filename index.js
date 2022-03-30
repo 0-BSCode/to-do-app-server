839dfe1f7372a53072ed0509915e05a165065ec3
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-03-30 11:05:24 +0800

    Connected API to Mongo database

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 
@@ -14,6 +15,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log("Server running!"));
-// mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-// .then;
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}!`)))
+.catch(error => console.log(error.message));
